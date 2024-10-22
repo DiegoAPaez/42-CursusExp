b2b4e45702bd08447534481ec7484bef3f23e4cf
DiegoAPaez <diego.p31@hotmail.com>
2024-10-22 12:06:45 +0200

    Adding get-next-line files

--- 01-get-next-line/get_next_line.h
+++ 01-get-next-line/get_next_line.h
@@ -6,13 +6,24 @@
 /*   By: dpaez <dpaez@student.42barcelona.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 10:27:39 by dpaez             #+#    #+#             */
-/*   Updated: 2024/10/08 10:42:59 by dpaez            ###   ########.fr       */
+/*   Updated: 2024/10/22 10:51:02 by dpaez            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
- #define GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
 
 char	*get_next_line(int fd);
+size_t	ft_strlen(const char *src);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strdup(const char *src);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
 
 #endif
